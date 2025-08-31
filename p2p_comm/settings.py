7a6a9355d51480b6f38e36749bc5c2eafe21e482
Ritik Kumar Sen <ritikk060@gmail.com>
2025-09-01 01:06:47 +0530

    Fixed Post username field

--- p2p_comm/settings.py
+++ p2p_comm/settings.py
@@ -77,7 +77,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = get_env(
     "CORS_ALLOWED_ORIGINS", 
-    default="http://localhost:3000,http://127.0.0.1:3000,http://localhost:8000,http://127.0.0.1:8000"
+    default="http://localhost:5173,http://127.0.0.1:3000,http://localhost:8000,http://127.0.0.1:8000"
 ).split(",")
 
 
