08eb94c2a991bf961df478f15e97ea37f36681a2
Soham-047 <129654959+Soham-047@users.noreply.github.com>
2025-09-10 00:44:09 +0530

    celery_fix

--- p2p_comm/celery.py
+++ p2p_comm/celery.py
@@ -1,17 +1,26 @@
 import os
+import ssl
 from celery import Celery
+from django.conf import settings
 
+# Default Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "p2p_comm.settings")
 
 app = Celery("p2p_comm")
-app.conf.update(timezone = 'Asia/Kolkata')
 
-# Load settings from Django
+# Load from Django settings with CELERY_ namespace
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Auto-discover tasks inside each app
+# SSL config if using rediss://
+if settings.REDIS_URL.startswith("rediss://"):
+    app.conf.update(
+        broker_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},   # or ssl.CERT_REQUIRED if needed
+        redis_backend_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
+    )
+
+# Auto-discover tasks
 app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+    print(f"Request: {self.request!r}")
