dc8ed9c26c0d833a92fdf35751042385b10408ee
Ubuntu <ubuntu@ip-172-31-6-170.ap-south-1.compute.internal>
2025-09-30 15:59:53 +0000

    fc

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,8 +25,5 @@ fi
 echo "Running migrations..."
 python manage.py migrate --noinput
 
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
 echo "Starting application..."
 exec "$@"
